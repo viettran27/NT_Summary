4120b225516b102aac43a3b8cbb42e2b037ecbfa
viettran27 <viettranduc8996@gmail.com>
2024-10-08 13:51:56 +0700

    config router

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { routes } from './constants/routes.jsx'
+import { Navigate } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route>
+          <Route path="/" element={<Navigate to={"/bao_cao_san_luong"} />} />
           <Route path="/" element={<App />}>
             {
               routes.map((route, index) => (
